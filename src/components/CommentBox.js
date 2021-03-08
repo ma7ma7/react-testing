6bb40a8de9638a88419dbd58fac4891171396a01
ma7ma7 <mahmoudamane5@gmail.comm>
2021-03-08 15:37:40 +0100

    Basic scafolding

---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CommentBox extends Component {
+  render() {
+    return <p>This is a comment box</p>;
+  }
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CommentList extends Component {
+  render() {
+    return <p>This is a comment list</p>;
+  }
+}
+
+export default CommentList;
