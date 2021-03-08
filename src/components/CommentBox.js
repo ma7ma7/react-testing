f03c09cab6d59451beb44ce50b4de1143c0665f4
ma7ma7 <mahmoudamane5@gmail.comm>
2021-03-08 17:21:52 +0100

    Testing CommentBox Functionalities

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      comment: event.target.value,
+    });
+  };
+
+  handelSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      comment: "",
+    });
+  };
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handelSubmit}>
         <h4>Enter Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <br />
         <button>Add</button>
       </form>
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox from "../CommentBox";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("it has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
+
+describe("the textarea", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" },
+    });
+    wrapped.update();
+  });
+
+  it("has a text area that a user can tyupe in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("when form is submitted the text area is emptied", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
